42d6105b944376dde479ec74c4024f96bd8c41f1
Vu Nguyen Duc Huy <soobin205304@gmail.com>
2026-01-19 10:16:33 +0700

    Du an mat kinh nay da co thay doi

--- src/main/java/vn/titv/spring/JPA/common/MyConfiguration.java
+++ src/main/java/vn/titv/spring/JPA/common/MyConfiguration.java
@@ -23,7 +23,7 @@ public class MyConfiguration {
                 if(choose == 1) {
                     // Gọi phương thức thêm sinh viên
                     // Lấy thông tin sinh viên
-                    System.out.println("Nhập họ đệm: ");
+                    System.out.println("Nhập họ đệm hihi: ");
                     String ho_dem = scanner.nextLine();
 
                     System.out.println("Nhập tên: ");
